dfaa82a22d129bbdeaf23f16d451fee4a66908ef
Diego Pineda <zentoo31@gmail.com>
2025-07-31 23:29:01 -0500

    Add Login layout and page components for user authentication

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Sign In - VaultChain"
+};
+
+function LoginLayout() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginLayout
