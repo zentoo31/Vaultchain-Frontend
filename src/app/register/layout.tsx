6f896f01b439456c14914aceda66a1ca72f734bd
Diego Pineda <zentoo31@gmail.com>
2025-07-31 23:29:05 -0500

    Add Register layout and page components for user registration

---
+++ src/app/register/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Register - VaultChain"
+};
+
+function RegisterLayout() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RegisterLayout
