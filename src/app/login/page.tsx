7f94748e1e5068137b130b0e4c9aec710e50e0fc
Diego Pineda <zentoo31@gmail.com>
2025-08-06 21:35:13 -0500

    Implement login functionality with success and error notifications

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import toast, { Toaster, ToastOptions } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import { AuthService } from '@/services/auth.service';
+import { toastGeneralOptions } from '@/utils/toastGeneralOptions';
+import { LoginDTO } from '@/dto/loginDTO';
 
 function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+  const authService = new AuthService();
+  const credentials: LoginDTO = {
+    email: '',
+    password: ''
+  };
+
+  const sucess = (message:string) => toast.success(message, toastGeneralOptions);
+  const error = (message: string) => toast.error(message, toastGeneralOptions);
+
+  const handleLogin = async (logintDTO: LoginDTO) => {
+    setIsLoading(true);
+    try {
+      const user = await authService.login(logintDTO);
+      sucess('Login successful!');
+      console.log('Logged in user:', user);
+    } catch (err) {
+      error(typeof err === 'string' ? err : 'Login failed. Please try again.');
+    } finally {
+      setIsLoading(false);
+    }
+  } 
+
+
   return (
     <div className='font-sans items-center justify-center min-h-screen p-8 pb-15 gap-16 sm:p-20'>
       <div className='text-2xl font-bold text-center mb-8'>
@@ -22,9 +52,11 @@ function Login() {
         <button
           type='submit'
           className='bg-blue-500 text-white p-2 rounded w-full max-w-md hover:bg-blue-600 transition-colors'
+          onClick={sucess.bind(null, "Login successful!")}
         >
           Login
         </button>
+        <Toaster />
       </form>
     </div>
   )
