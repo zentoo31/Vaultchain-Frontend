fbcd1cf0a73c624176c45f25874e8d07599c2017
Diego Pineda <zentoo31@gmail.com>
2025-08-16 20:50:12 -0500

    Add DashboardLayout component with metadata for the dashboard page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+    title: "Dashboard - Vaultchain"
+};
+
+function DashboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
+
+export default DashboardLayout;
