3c617770bd0d1b1f8e5c591d0361d06611616c3e
Diego Pineda <zentoo31@gmail.com>
2025-08-16 20:50:16 -0500

    Add Dashboard component with layout and styling for the dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+function Dashboard() {
+  return (
+    <div className='font-sans items-center justify-center min-h-screen p-8 pb-15 gap-16 sm:p-20'>
+        <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard;
