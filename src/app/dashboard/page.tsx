9577342d51a984981c027131ee33729f19d1e506
zentoo31 <zentoo31@gmail.com>
2025-08-21 18:45:24 -0500

    Implement Dashboard component with profile fetching and display

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,29 @@
 "use client"
+import { ProfileService } from "@/services/profile.service";
+import { ProfileInfo } from "@/interfaces/profile-info";
+import { useEffect, useState } from "react";
+
 function Dashboard() {
+  const profileService = new ProfileService();
+  const [profile, setProfile] = useState<ProfileInfo | null>(null);
+
+  const getProfileInfo= async() =>{
+    try {
+      const profileData = await profileService.getProfileInfo();
+      setProfile(profileData);
+    } catch (error) {
+      console.error('Error fetching profile:', error);
+    }
+  }
+
+  useEffect(() => {
+    getProfileInfo();
+  }, []);
+
   return (
     <div className='font-sans items-center justify-center min-h-screen p-8 pb-15 gap-16 sm:p-20'>
         <h1>Dashboard</h1>
+        <h2>BTC Adresss: {profile?.wallet.bitcoinAddress}</h2>
     </div>
   )
 }
