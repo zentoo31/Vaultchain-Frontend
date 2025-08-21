8664700c39acaa1dc5dfcf8211752bfd38ac51d0
zentoo31 <zentoo31@gmail.com>
2025-08-21 18:22:00 -0500

    Implement ProfileService with API integration for fetching user profile information and error handling

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,3 +1,19 @@
+import axios, { AxiosError } from "axios";
+import { handleError } from "@/utils/errorHandler";
+import { ProfileInfo } from "@/interfaces/profile-info";
+
 export class ProfileService {
+    private readonly apiUrl: string = `${process.env.API_BACKEND_URL}/profile` || 'http://localhost:4321/profile';
     
+    async getProfileInfo(): Promise<ProfileInfo> {
+        try {
+            const response = await axios.get<ProfileInfo>(`${this.apiUrl}/info`, { 
+                withCredentials: true 
+            });
+            return response.data;
+        } catch (error){
+            const axiosError = error as AxiosError;
+            throw handleError(axiosError);
+        }
+    }
 }
